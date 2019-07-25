782031ae405b5e5f11ad269a055990d139d3a66c
Chris Cowan <agentme49@gmail.com>
2019-07-24 17:26:48 -0700

    add typescript type def

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import KefirDefault from 'kefir';
+import * as Kefir from 'kefir';
+
+export default function kefirCast(_Kefir: typeof Kefir | typeof KefirDefault, input: any): Kefir.Observable<any,any>;
